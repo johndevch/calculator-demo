a73bd18cf8423a8257f9a6e429b7e1b3a6cccf82
johndevch <60101009+johndevch@users.noreply.github.com>
2020-01-20 13:36:41 +0000

    Update arithmeticController.js
    because

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -12,7 +12,8 @@ exports.calculate = function(req, res) {
 
   var operations = {
     // remove extra plus signs
-    'add':      function(a,b) { return a + b },
+    // Extra plussed need for conversion
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
